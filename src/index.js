af0d4062e3e706f8670e9bae4b32c5c8ddadad14
github username <github email address>
2017-06-02 10:19:32 -0400

    not working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-  // your code here!
+  $('#submit').on('click', function(e){
+    e.preventDefault();
+    var picture = $('#img').val
+    var caption = $('#caption').val
+    $('#photo-list').html(`<img src="${picture}" caption="${caption}"/>`)
+  })
 })
